02e9b184fd3e5d5fbaa6bd16e30f3766fbafe846
KowsarAtz <kowsar.atazadeh@gmail.com>
2019-08-25 15:18:36 +0430

    mysql.connector module usage worked

---
+++ mssql2mariadb.py
@@ -0,0 +1,20 @@
+import pymongo
+import pymssql
+import mysql.connector
+
+conn = pymssql.connect(server='172.16.8.10\RICESTSQLSERVER', user='sa', password='RICEST@SQLSERVER2008', database='NOSQL_db')  
+cursor = conn.cursor()
+
+mysql_connection = mysql.connector.connect(host='localhost', user="root", passwd="password", db='nosqlprj')
+cursor2 = mysql_connection.cursor()
+
+ 
+cursor.execute('SELECT ID, DocID, Abstract FROM [NOSQL_db].[dbo].[LangFilter]') #2 Mil Records
+
+sum = 0
+
+for item in cursor:
+    cursor2.execute("INSERT INTO records (_id, DocID, Abstract) VALUES (%s, %s, %s)" , (str(item[0]), str(item[1]), item[2]))
+    sum+=1
+    print(sum, "items migrated.")
+mysql_connection.commit()
---
+++ sql2es.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from elasticsearch import Elasticsearch
+import pymssql
+
+SQL_EXPORT_DB = 'nosqlprj'
+RECORDS_COL = 'records'
+
+es = Elasticsearch('172.16.13.26')
+
+conn = pymssql.connect(server='172.16.8.10\RICESTSQLSERVER', user='sa', password='RICEST@SQLSERVER2008', database='NOSQL_db')  
+cursor = conn.cursor()
+
+cursor.execute('SELECT ID, DocID, Abstract FROM [NOSQL_db].[dbo].[LangFilter]')
+
+sum = 0
+
+for item in cursor:
+    new_dict = {}
+    new_dict['DocID'] = item[1]
+    new_dict['Abstract'] = item[2]
+    res = es.index(index=SQL_EXPORT_DB, doc_type=RECORDS_COL, id=item[0], body=new_dict)
+    sum+=1
+    print(sum, "items migrated to ES.")
+
+es.indices.refresh(index=SQL_EXPORT_DB)
