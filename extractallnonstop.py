bc0f415e98cb06d81812fdc6fa2664ed7fd9398c
KowsarAtz <kowsar.atazadeh@gmail.com>
2019-08-27 12:42:13 +0430

    random word

--- extractallnonstop.py
+++ extractallnonstop.py
@@ -1,12 +1,29 @@
 import pymssql
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize 
+import random
+
+SOURCE_TABLE = "[NOSQL_db].[dbo].[LangFilter]"
+NUMBER_OF_RECORDS = 2000000
+STOPWORDS = set(stopwords.words('english'))
 
 conn = pymssql.connect(server='172.16.8.10\RICESTSQLSERVER', user='sa', password='RICEST@SQLSERVER2008', database='NOSQL_db')  
 cursor = conn.cursor()
-cursor.execute('SELECT TOP (50) ID, DocID, Abstract FROM [NOSQL_db].[dbo].[LangFilter]') #2 Mil Records
 
-sum=0
-with open("allabstracts.txt", "a") as myfile:
+def containnumber(word):
+    for i in range(0, 10):
+        if str(i) in word:
+            return True
+    return False
+
+def randomword(n):
+    cmd = "SELECT Abstract FROM (SELECT ROW_NUMBER() OVER (ORDER BY ID ASC) AS rownumber, Abstract FROM %s) AS foo WHERE rownumber = %d"  % (SOURCE_TABLE, random.randint(1, NUMBER_OF_RECORDS)) 
+    cursor.execute(cmd)
     for item in cursor:
-        myfile.write("\n%s" % item[2])
-        sum+=1
-        print("Total: %d" % sum)
+        word_tokens = word_tokenize(item[0])
+    w = word_tokens[random.randint(0, len(word_tokens)-1)]
+    if (len(w) >= n and (w.lower() not in STOPWORDS) and not containnumber(w)):
+        return w.lower()
+    return randomword(n)
+
+
--- mssql2mariadb.py
+++ mssql2mariadb.py
@@ -12,9 +12,13 @@ cursor2 = mysql_connection.cursor()
 cursor.execute('SELECT ID, DocID, Abstract FROM [NOSQL_db].[dbo].[LangFilter]') #2 Mil Records
 
 sum = 0
-
+bulk = 0
+partition = 0
 for item in cursor:
     cursor2.execute("INSERT INTO records (_id, DocID, Abstract) VALUES (%s, %s, %s)" , (str(item[0]), str(item[1]), item[2]))
     sum+=1
-    print(sum, "items migrated.")
-mysql_connection.commit()
+    if sum == 1000:
+        sum = 0
+        bulk+=1
+        mysql_connection.commit()
+        print("Bulk Count: %d" % bulk)
