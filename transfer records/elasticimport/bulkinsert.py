3ad242f52b4695b7ac650ffa601114f491fd7d1e
KowsarAtz <kowsar.atazadeh@gmail.com>
2019-09-04 12:01:38 +0430

    es clusters bulk insert

--- transfer records/elasticimport/bulkinsert.py
+++ transfer records/elasticimport/bulkinsert.py
@@ -5,19 +5,28 @@ from elasticsearch import helpers
 
 BULK_SIZE = 50000
 
-es = Elasticsearch('172.16.13.26:9200')
+es = Elasticsearch('172.16.13.26:9206')
 
 conn = pymssql.connect(server='172.16.8.10\RICESTSQLSERVER', user='sa', password='RICEST@SQLSERVER2008', database='NOSQL_db')  
 cursor = conn.cursor()
 
-cursor.execute('SELECT TOP (3000000) ID, DocID, Abstract FROM [NOSQL_db].[dbo].[LangFilterIEEE]') # 3 Mil
+cursor.execute('SELECT ID, DocID, Abstract FROM [NOSQL_db].[dbo].[LangFilter]')
 sum = 0
 bulks = 0
 actions = []
 for item in cursor:
     actions += [
         {
-            "_index": "nosqlprj",
+            "_index": "nosqlprj-3shard",
+            "_type": "records",
+            "_id": item[0],
+            "_source": {
+                "DocID": item[1],
+                "Abstract": item[2]
+            }
+        },
+        {
+            "_index": "nosqlprj-6shard",
             "_type": "records",
             "_id": item[0],
             "_source": {
@@ -34,4 +43,35 @@ for item in cursor:
         actions = []
         print ("\rNo of bulks inserted: %d" % bulks, end="")
 
-
+cursor.execute('SELECT TOP (3000000) ID, DocID, Abstract FROM [NOSQL_db].[dbo].[LangFilterIEEE]') # 3 Mil
+sum = 0
+bulks = 0
+actions = []
+for item in cursor:
+    actions += [
+        {
+            "_index": "nosqlprj-3shard",
+            "_type": "records",
+            "_id": item[0],
+            "_source": {
+                "DocID": item[1],
+                "Abstract": item[2]
+            }
+        },
+        {
+            "_index": "nosqlprj-6shard",
+            "_type": "records",
+            "_id": item[0],
+            "_source": {
+                "DocID": item[1],
+                "Abstract": item[2]
+            }
+        }
+    ]
+    sum+=1
+    if sum == BULK_SIZE:
+        bulks += 1
+        sum = 0
+        helpers.bulk(es, actions)
+        actions = []
+        print ("\rNo of IEEE bulks inserted: %d" % bulks, end="")
