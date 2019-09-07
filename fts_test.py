48c1171b04bac1d0c5db4e891df02f341fd78385
KowsarAtz <kowsar.atazadeh@gmail.com>
2019-09-07 16:11:14 +0430

    about to start mongo test

--- fts_test.py
+++ fts_test.py
@@ -1,12 +1,12 @@
 from newutils import *
 
-es5_client = Elasticsearch5(HOSTIP + ':9205', timeout=120)
-es5_client_3shard = Elasticsearch5(HOSTIP + ':9206', timeout=120)
+# es5_client = Elasticsearch5(HOSTIP + ':9205', timeout=120)
+# es5_client_3shard = Elasticsearch5(HOSTIP + ':9206', timeout=120)
 mongo_client = pymongo.MongoClient('mongodb://%s:27023/' % HOSTIP)
 mongo_client_3sh = pymongo.MongoClient('mongodb://%s:27030/' % HOSTIP)
 mongo_client_6sh = pymongo.MongoClient('mongodb://%s:27032/' % HOSTIP)
-mssql_client = pymssql.connect(server=HOSTIP, user='sa', password='MSSql-pwd', database=DB)
-mysql_client = mysql.connector.connect(host='localhost', user="root", passwd="password", db=DB)
+# mssql_client = pymssql.connect(server=HOSTIP, user='sa', password='MSSql-pwd', database=DB)
+# mysql_client = mysql.connector.connect(host='localhost', user="root", passwd="password", db=DB)
 
 def runworkload(db, optype, ref, conn):
     times = []
@@ -44,12 +44,15 @@ def runworkload(db, optype, ref, conn):
     mean = sum(times)/len(times)
     return mean*1000
 
-SRC = "randomwordstemp.txt"
-SRCP = "randomphrasetemp.txt"
-# OPS = [SINGLE, AND, OR ,EXACTPHRASE]
-OPS = [AND]
-DBS = [(ELASTIC5, es5_client), (ELASTIC5_3, es5_client_3shard), (ELASTIC5_6, es5_client_3shard)]
-DBS += [(MSSQL, mssql_client), (MYSQL, mysql_client)]
+SRC = "randomwords500.txt"
+SRCP = "randomphrase500.txt"
+OPS = [SINGLE, AND, OR ,EXACTPHRASE]
+DBS = []
+# OPS = [OR]
+# DBS += [(MSSQL, mssql_client)]
+# DBS += [(ELASTIC5, es5_client), (ELASTIC5_3, es5_client_3shard), (ELASTIC5_6, es5_client_3shard)]
+# DBS += [(MSSQL, mssql_client), (MYSQL, mysql_client)]
+# DBS += [(MYSQL, mysql_client)]
 DBS += [(MONGODB, mongo_client), (MONGODB_3, mongo_client_3sh), (MONGODB_6, mongo_client_6sh)]
 
 for db in DBS:
