dfd4214f6195caca3b2f8525cbc948fb0858ffe9
KowsarAtz <kowsar.atazadeh@gmail.com>
2019-09-07 11:42:13 +0430

    -

--- fts_test.py
+++ fts_test.py
@@ -1,13 +1,12 @@
 from newutils import *
 
-# es5_client = Elasticsearch5(HOSTIP + ':9205')
-# es5_client_3shard = Elasticsearch5(HOSTIP + ':9206')
-# es7_client = Elasticsearch(HOSTIP)
+# es5_client = Elasticsearch5(HOSTIP + ':9205', timeout=120)
+es5_client_3shard = Elasticsearch5(HOSTIP + ':9206', timeout=120)
 # mongo_client = pymongo.MongoClient('mongodb://%s:27023/' % HOSTIP)
 # mongo_client_3sh = pymongo.MongoClient('mongodb://%s:27030/' % HOSTIP)
 mongo_client_6sh = pymongo.MongoClient('mongodb://%s:27032/' % HOSTIP)
-# mssql_client = pymssql.connect(server=HOSTIP, user='sa', password='MSSql-pwd', database=DB)
-# mysql_client = mysql.connector.connect(host='localhost', user="root", passwd="password", db=DB)
+mssql_client = pymssql.connect(server=HOSTIP, user='sa', password='MSSql-pwd', database=DB)
+mysql_client = mysql.connector.connect(host='localhost', user="root", passwd="password", db=DB)
 
 def runworkload(db, optype, ref, conn):
     times = []
@@ -49,10 +48,15 @@ def runworkload(db, optype, ref, conn):
 # OPS = [SINGLE, AND, OR]
 # DBS = [(MSSQL, mssql_client), (MYSQL, mysql_client), (MONGODB, mongo_client), (ELASTIC5, es5_client)]
 SRC = "randomwordstemp.txt"
-OPS = [EXACTPHRASE]
-DBS = [(MONGODB_6, mongo_client_6sh)]
+SRCP = "randomphrasetemp.txt"
+OPS = [SINGLE, AND, OR ,EXACTPHRASE]
+DBS = [(MONGODB_6, mongo_client_6sh), (ELASTIC5_6, es5_client_3shard), (MSSQL, mssql_client), (MYSQL, mysql_client)]
 
 for db in DBS:
     for op in OPS:
-        mean = runworkload(db[0], op, SRC, db[1])
+        if op == EXACTPHRASE:
+            src = SRCP
+        else:
+            src = SRC
+        mean = runworkload(db[0], op, src, db[1])
         print(" | AvgLatency: %f ms" % mean)
