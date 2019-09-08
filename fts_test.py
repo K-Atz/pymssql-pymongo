501eb3757580dad9cd81c49609f7f59c9a171315
KowsarAtz <kowsar.atazadeh@gmail.com>
2019-09-08 15:14:05 +0430

    mariadb done

--- fts_test.py
+++ fts_test.py
@@ -3,8 +3,8 @@ from newutils import *
 # es5_client = Elasticsearch5(HOSTIP + ':9205', timeout=120)
 # es5_client_3shard = Elasticsearch5(HOSTIP + ':9206', timeout=120)
 # mongo_client = pymongo.MongoClient('mongodb://%s:27023/' % HOSTIP)
-mongo_client_3sh = pymongo.MongoClient('mongodb://%s:27030/' % HOSTIP)
-# mongo_client_6sh = pymongo.MongoClient('mongodb://%s:27032/' % HOSTIP)
+# mongo_client_3sh = pymongo.MongoClient('mongodb://%s:27030/' % HOSTIP)
+mongo_client_6sh = pymongo.MongoClient('mongodb://%s:27032/' % HOSTIP)
 # mssql_client = pymssql.connect(server=HOSTIP, user='sa', password='MSSql-pwd', database=DB)
 mysql_client = mysql.connector.connect(host='localhost', user="root", passwd="password", db=DB)
 
@@ -44,12 +44,12 @@ def runworkload(db, optype, ref, conn):
     mean = sum(times)/len(times)
     return mean*1000
 
-SRCOR = "./newRandomSeries-150/randomWordsOR.txt"
-SRCP = "./newRandomSeries-150/randomPhrase.txt"
-SRCAND = "./newRandomSeries-150/randomWordsAND.txt"
+SRCOR = "./newRandomSeries-70/randomWordsOR.txt"
+SRCP = "./newRandomSeries-70/randomPhrase.txt"
+SRCAND = "./newRandomSeries-70/randomWordsAND.txt"
 OPS = []
 # OPS += [SINGLE, AND, OR ,EXACTPHRASE]
-OPS += [OR, EXACTPHRASE]
+OPS += [AND, OR, EXACTPHRASE]
 DBS = []
 # DBS += [(ELASTIC5, es5_client), (ELASTIC5_3, es5_client_3shard), (ELASTIC5_6, es5_client_3shard)]
 # DBS += [(MSSQL, mssql_client), (MYSQL, mysql_client)]
@@ -57,8 +57,8 @@ DBS = []
 # DBS += [(MONGODB, mongo_client), (MONGODB_3, mongo_client_3sh), (MONGODB_6, mongo_client_6sh)]
 # DBS += [(MSSQL, mssql_client)]
 # DBS += [(ELASTIC5_3, es5_client_3shard), (ELASTIC5_6, es5_client_3shard)]
-DBS += [(MONGODB_3, mongo_client_3sh)]
-# DBS += [(MONGODB_6, mongo_client_6sh)]
+# DBS += [(MONGODB_3, mongo_client_3sh)]
+DBS += [(MONGODB_6, mongo_client_6sh)]
 
 for op in OPS:
     for db in DBS:
